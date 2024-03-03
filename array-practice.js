c98eac0432b5e7f4967d72e8fff072797d80b85d
dcsiona@gmail.com <dcsiona@gmail.com>
2024-03-03 07:49:49 -0500

    another commit

--- array-practice.js
+++ array-practice.js
@@ -26,7 +26,51 @@ regardless of the input array' size.
 const runningSum = arr => {
 
   // Your code here
+  // Initializes an empty arr to store the running sum.
+  const result = [];
+  // Initializes sum to zero.
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    // add the current element to the sum
+    sum += arr[i];
+    // push the updated sum to the result arr
+    result.push(sum);
+  }
+  return result;
 };
+
+const arr = [1, 2, 3, 4];
+console.log(runningSum(arr));
+
+// [ 1, 3, 6, 10 ]
+
+/*
+Time complexity: O(n) Linear time.
+The loop iterates through the entire array once, performing a constant time addition
+for each element.
+Space complexity: O(n) Linear time.
+The function creates a new arr result, the size of the arr is equal to the original
+arr' size, therefore the space complexity is also linear.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const evenNumOfChars = arr => {
 
@@ -55,4 +99,4 @@ const shuffle = (arr) => {
 };
 
 
-module.exports = [findMinimum, runningSum, evenNumOfChars, smallerThanCurr, twoSum, secondLargest, shuffle];
+module.exports = [runningSum, evenNumOfChars, smallerThanCurr, twoSum, secondLargest, shuffle];
